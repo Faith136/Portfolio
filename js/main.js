49bf6d182a25bf9c5af806073c706d49fc58d508
Faith136 <faithodongo9@gmail.com>
2023-11-28 10:02:27 +0300

    first

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ const menu = document.querySelector('.m-list');
 btn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
+
+
+function sendEmail() {
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "faithmecha2@gmail.com",
+        Password: "EF09A33A5F3DDDFE86E95957E3E02563B29D",
+        To: 'faithmecha2.com',
+        From: document.getElementById("email").value,
+        Subject: "New Portfolio Inquiry",
+        Body: "And this is the body"
+    }).then(
+        message => alert(message)
+    );
+}
